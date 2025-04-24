3c13a50096c5696ab2f05135c3a73ec6323da9cb
iworkforpurpose <122251065+iworkforpurpose@users.noreply.github.com>
2025-04-24 17:54:56 +0530

    Update esrgan_model.py

--- src/esrgan_model.py
+++ src/esrgan_model.py
@@ -3,31 +3,32 @@ import torch.nn as nn
 import torchvision.models as models
 
 
-# Dense block inside RRDB
+# ----------------------------
+# Dense Block (used inside RRDB)
+# ----------------------------
 class DenseBlock(nn.Module):
-    def __init__(self, channels=64, growth_channels=32):
+    def __init__(self, in_channels=64, growth_channels=32):
         super(DenseBlock, self).__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(channels, growth_channels, 3, 1, 1),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(channels + growth_channels, growth_channels, 3, 1, 1),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(channels + 2 * growth_channels, growth_channels, 3, 1, 1),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(channels + 3 * growth_channels, growth_channels, 3, 1, 1),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(channels + 4 * growth_channels, channels, 3, 1, 1),
-        )
+        self.lrelu = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv1 = nn.Conv2d(in_channels, growth_channels, 3, 1, 1)
+        self.conv2 = nn.Conv2d(in_channels + growth_channels, growth_channels, 3, 1, 1)
+        self.conv3 = nn.Conv2d(in_channels + 2 * growth_channels, growth_channels, 3, 1, 1)
+        self.conv4 = nn.Conv2d(in_channels + 3 * growth_channels, growth_channels, 3, 1, 1)
+        self.conv5 = nn.Conv2d(in_channels + 4 * growth_channels, in_channels, 3, 1, 1)
 
     def forward(self, x):
-        inputs = x
-        for layer in self.block:
-            out = layer(inputs)
-            inputs = torch.cat([inputs, out], 1) if isinstance(layer, nn.Conv2d) else out
-        return inputs * 0.2 + x
+        c1 = self.lrelu(self.conv1(x))
+        c2 = self.lrelu(self.conv2(torch.cat([x, c1], 1)))
+        c3 = self.lrelu(self.conv3(torch.cat([x, c1, c2], 1)))
+        c4 = self.lrelu(self.conv4(torch.cat([x, c1, c2, c3], 1)))
+        c5 = self.conv5(torch.cat([x, c1, c2, c3, c4], 1))
+        return x + 0.2 * c5
 
 
-# Residual in Residual Dense Block
+# ----------------------------
+# RRDB Block (Residual in Residual Dense Block)
+# ----------------------------
 class RRDB(nn.Module):
     def __init__(self, channels):
         super(RRDB, self).__init__()
@@ -38,30 +39,31 @@ class RRDB(nn.Module):
         )
 
     def forward(self, x):
-        return x + self.block(x) * 0.2
+        return x + 0.2 * self.block(x)
 
 
+# ----------------------------
 # ESRGAN Generator
+# ----------------------------
 class GeneratorRRDB(nn.Module):
-    def __init__(self, in_channels=3, out_channels=3, channels=64, num_blocks=23):
+    def __init__(self, in_channels=3, out_channels=3, channels=64, num_blocks=23, scale=4):
         super(GeneratorRRDB, self).__init__()
         self.initial = nn.Conv2d(in_channels, channels, 3, 1, 1)
 
-        # 23 RRDB blocks
+        # Trunk: 23 RRDB blocks
         self.trunk = nn.Sequential(*[RRDB(channels) for _ in range(num_blocks)])
         self.trunk_conv = nn.Conv2d(channels, channels, 3, 1, 1)
 
         # Upsampling
-        self.upsample = nn.Sequential(
-            nn.Conv2d(channels, channels * 4, 3, 1, 1),
-            nn.PixelShuffle(2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(channels, channels * 4, 3, 1, 1),
-            nn.PixelShuffle(2),
-            nn.LeakyReLU(0.2, inplace=True)
-        )
+        upsample_layers = []
+        for _ in range(int(scale).bit_length() - 1):
+            upsample_layers += [
+                nn.Conv2d(channels, channels * 4, 3, 1, 1),
+                nn.PixelShuffle(2),
+                nn.LeakyReLU(0.2, inplace=True)
+            ]
+        self.upsample = nn.Sequential(*upsample_layers)
 
-        # Final conv
         self.final = nn.Conv2d(channels, out_channels, 3, 1, 1)
 
     def forward(self, x):
@@ -69,11 +71,12 @@ class GeneratorRRDB(nn.Module):
         trunk = self.trunk_conv(self.trunk(fea))
         fea = fea + trunk
         out = self.upsample(fea)
-        out = self.final(out)
-        return out
+        return self.final(out)
 
 
-# Reuse existing discriminator
+# ----------------------------
+# ESRGAN Discriminator
+# ----------------------------
 class Discriminator(nn.Module):
     def __init__(self, input_shape=(3, 96, 96)):
         super(Discriminator, self).__init__()
@@ -95,6 +98,7 @@ class Discriminator(nn.Module):
         layers.extend(d_block(256, 256, stride=2))
         layers.extend(d_block(256, 512))
         layers.extend(d_block(512, 512, stride=2))
+
         layers.append(nn.AdaptiveAvgPool2d((6, 6)))
         layers.append(nn.Flatten())
         layers.append(nn.Linear(512 * 6 * 6, 1024))
@@ -107,11 +111,16 @@ class Discriminator(nn.Module):
         return self.model(x)
 
 
-# VGG feature extractor (before activation)
+# ----------------------------
+# VGG Feature Extractor (for perceptual loss)
+# ----------------------------
 class VGGFeatureExtractor(nn.Module):
     def __init__(self, layer=34, use_bn=False, device='cpu'):
         super(VGGFeatureExtractor, self).__init__()
-        vgg = models.vgg19_bn(weights=models.VGG19_BN_Weights.DEFAULT) if use_bn else models.vgg19(weights=models.VGG19_Weights.DEFAULT)
+        if use_bn:
+            vgg = models.vgg19_bn(weights=models.VGG19_BN_Weights.DEFAULT)
+        else:
+            vgg = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
         self.features = nn.Sequential(*list(vgg.features.children())[:layer]).to(device)
         for p in self.features.parameters():
             p.requires_grad = False
